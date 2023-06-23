21f9cc6f8cbf9e44afb052ba571fd46ffa856642
asus_divedi <137493610+asusdivedi@users.noreply.github.com>
2023-06-23 14:47:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title><style>button{
+        border:2px solid red;
+        color:green;
+
+    }</style>
+</head>
+<body>
+    <h1>i am ritesh</h1><button>hero</button>
+    <?php echo "ritesh here";
+    $a=40;
+    $sname="divedi";
+    $b=20;
+    echo $a+$b;
+    echo"<br>";
+    echo "<button>",$sname;
+    
+    
+    
+    
+    
+    ?>
+</body>
+</html>
